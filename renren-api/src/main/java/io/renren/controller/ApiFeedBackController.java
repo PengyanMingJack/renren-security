cc5dc80bec780998dfdfa7a137cbba498d12cd07
yxf <1071682016@qq.com>
2019-08-17 21:41:42 +0800

    意见反馈

--- renren-api/src/main/java/io/renren/controller/ApiFeedBackController.java
+++ renren-api/src/main/java/io/renren/controller/ApiFeedBackController.java
@@ -1,6 +1,8 @@
 package io.renren.controller;
 
 import io.renren.common.utils.R;
+import io.renren.common.validator.ValidatorUtils;
+import io.renren.entity.FeedBackEntity;
 import io.renren.form.FeedBackFrom;
 import io.renren.service.FeedBackService;
 import io.swagger.annotations.Api;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/api")
@@ -22,7 +26,13 @@ public class ApiFeedBackController {
     @PostMapping("feedBack")
     @ApiOperation("意见反馈")
     public R feedBack(@RequestBody FeedBackFrom from) {
-        feedBackService.save(from);
+        ValidatorUtils.validateEntity(from);
+
+        FeedBackEntity feedBack = new FeedBackEntity();
+        feedBack.setUserId(from.getUserId());
+        feedBack.setContent(from.getContent());
+        feedBack.setCreateTime(new Date());
+        feedBackService.save(feedBack);
 
         return R.ok();
 
